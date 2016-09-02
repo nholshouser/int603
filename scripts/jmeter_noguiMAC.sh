5e0f2d6289df1ee31c81e3fda1c844ef5f63a8c4
nholshouser <nicholas.holshouser@sap.com>
2016-09-02 16:02:07 -0400

    updates for MAC

--- scripts/jmeter_noguiMAC.sh
+++ scripts/jmeter_noguiMAC.sh
@@ -9,17 +9,19 @@
 #MAC
 USER=`whoami`
 DATE=$(/bin/date +%Y%m%d_%H%M)
-TESTSCRIPT=/Users/${USER}/GIT/int603/scripts/INT603_XITESTMAC.jmx
+TESTSCRIPT=/Users/${USER}/GIT/int603/scripts/INT603_XITEST_MAC.jmx
 GRAPHSCRIPT=/Users/${USER}/GIT/int603/scripts/INT603_GraphsGenerator.jmx
-DIROUTPUT=/Users/${USER}/GIT/int603/csv/${DATE}_${SCENARIO}
-TESTRESULTS=${DIROUTPUT}/${SCENARIO}.csv
-PERFRESULTS=${DIROUTPUT}/PERFMON.csv
 
 
 echo -e "Input test scenario name followed by [ENTER]:"
 read SCENARIO
 echo "SCENARIO=${SCENARIO}"
-PREFIX=${SCENARIO}-
+
+# Set DIROUTPUT and TESTRESULTS and PERFRESULTS
+DIROUTPUT=/Users/${USER}/GIT/int603/csv/${DATE}_${SCENARIO}
+TESTRESULTS=${DIROUTPUT}/${SCENARIO}.csv
+PERFRESULTS=${DIROUTPUT}/PERFMON.csv
+FILEPREFIX=${SCENARIO}-
 
 echo -e "Input IP Address of Server to test"
 read TESTIP
@@ -27,8 +29,9 @@ echo "Server IP: ${TESTIP}"
 
 # Export variables
 export DIROUTPUT
-export DIROUTPUT
 export TESTRESULTS
+export GRAPHSCRIPT
+export PERFRESULTS
 export PREFIX
 export TESTIP
 
@@ -39,16 +42,16 @@ echo "Creating directory ${DIROUTPUT}"
 mkdir ${DIROUTPUT}
 
 # Run the test
-echo "Running command: jmeter30/apache-jmeter.3.0/bin/jmeter -t ${TESTSCRIPT} -n -l ${TESTRESULTS} -JDIROUTPUT=${DIROUTPUT} -JSCENARIO=${SCENARIO} -JTESTIP=${TESTIP} -JPREFIX=${PREFIX}"
-jmeter30/apache-jmeter-3.0/bin/jmeter -t ${TESTSCRIPT} -n -l ${TESTRESULTS} -JDIROUTPUT=${DIROUTPUT} -JSCENARIO=${SCENARIO} -JTESTIP=${TESTIP} -JPREFIX=${PREFIX}
+echo "Running command: jmeter30/apache-jmeter.3.0/bin/jmeter -t ${TESTSCRIPT} -n -l ${TESTRESULTS} -JDIROUTPUT=${DIROUTPUT} -JSCENARIO=${SCENARIO} -JTESTIP=${TESTIP} -JPERFRESULTS=${PERFRESULTS}"
+jmeter30/apache-jmeter-3.0/bin/jmeter -t ${TESTSCRIPT} -n -l ${TESTRESULTS} -JDIROUTPUT=${DIROUTPUT} -JSCENARIO=${SCENARIO} -JTESTIP=${TESTIP} -JPERFRESULTS=${PERFRESULTS}
 
 
 # Generate the JMETER graphs
 echo "Generate Graphs: jmeter30/apache-jmeter-3.0/bin/jmeter -t ${GRAPHSCRIPT} -n -l output.csv -JTESTRESULTS=${TESTRESULTS} -JDIROUTPUT=${DIROUTPUT} -JFILEPREFIX=${PREFIX}"
-jmeter30/apache-jmeter-3.0/bin/jmeter -t ${GRAPHSCRIPT} -n -l output.csv -JTESTRESULTS=${TESTRESULTS} -JDIROUTPUT=${DIROUTPUT} -JFILEPREFIX=${PREFIX}
+jmeter30/apache-jmeter-3.0/bin/jmeter -t ${GRAPHSCRIPT} -n -l output.csv -JTESTRESULTS=${TESTRESULTS} -JDIROUTPUT=${DIROUTPUT} -JFILEPREFIX=${FILEPREFIX}
 
 # Generate PERFMON Graphs
-echo "Generate Perf Graphs: java -jar jmeter30/apache-jmeter-3.0/lib/cmdrunner-2.0.jar --tool Reporter --generate-png CPU.png --input-jtl PERFMON.csv  --plugin-type PerfMon --width 800 --height 600 --auto-scale yes --relative-times no --include-labels \".*CPU.*\" --include-label-regex true"
+echo "Generate Perf Graphs: java -jar jmeter30/apache-jmeter-3.0/lib/cmdrunner-2.0.jar --tool Reporter --generate-png CPU.png --input-jtl ${PERFRESULTS}}  --plugin-type PerfMon --width 800 --height 600 --auto-scale yes --relative-times no --include-labels \".*CPU.*\" --include-label-regex true"
 java -jar jmeter30/apache-jmeter-3.0/lib/cmdrunner-2.0.jar --tool Reporter --generate-png ${DIROUTPUT}/CPU.png --input-jtl ${PERFRESULTS}  --plugin-type PerfMon --width 800 --height 600 --auto-scale yes --relative-times no --include-labels ".*CPU.*" --include-label-regex true
-echo "Generate Perf Graphs: java -jar jmeter30/apache-jmeter-3.0/lib/cmdrunner-2.0.jar --tool Reporter --generate-png Memory.png --input-jtl PERFMON.csv  --plugin-type PerfMon --width 800 --height 600 --auto-scale yes --relative-times no --include-labels \".*Memory.*\" --include-label-regex true"
+echo "Generate Perf Graphs: java -jar jmeter30/apache-jmeter-3.0/lib/cmdrunner-2.0.jar --tool Reporter --generate-png Memory.png --input-jtl ${PERFRESULTS}  --plugin-type PerfMon --width 800 --height 600 --auto-scale yes --relative-times no --include-labels \".*Memory.*\" --include-label-regex true"
 java -jar jmeter30/apache-jmeter-3.0/lib/cmdrunner-2.0.jar --tool Reporter --generate-png ${DIROUTPUT}/Memory.png --input-jtl ${PERFRESULTS}  --plugin-type PerfMon --width 800 --height 600 --auto-scale yes --relative-times no --include-labels ".*Memory.*" --include-label-regex true
--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@
 # scrtipts/* need 'chmod 755'
 # chmod 755 jmeter30/apache-jmeter-3.0/bin/jmeter
 
-echo "###################################################
+echo "###################################################"
 echo "Setting up directory for testing"
 echo "Create csv directory"
 mkdir csv
 ls -al | grep csv
 
-echo "###################################################
+echo "###################################################"
 echo "Setting permissions on certificates"
 chmod 600 CERTS/*.ppk
 chmod 600 CERTS/*.pem
@@ -23,11 +23,16 @@ echo "Setting permissions on all scripts"
 chmod 755 scripts/*.sh
 ls -al scripts
 
-echo "###################################################
+echo "###################################################"
+echo "Unzipping JMeter"
+unzip jmeter30.zip
+
+echo "###################################################"
 echo "Setting permission on jmeter30/apache-jmeter-3.0/bin/jmeter"
 chmod 755 jmeter30/apache-jmeter-3.0/bin/jmeter
+chmod 755 jmeter30/apache-jmeter-3.0/bin/jmeter.sh
 
-echo "###################################################
+echo "###################################################"
 echo "All setup complete"
 
 
