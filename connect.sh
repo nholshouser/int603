9aa12aed84f980855687898b6c24d00fcd40ad8b
nholshouser <nicholas.holshouser@sap.com>
2016-10-04 06:38:21 -0400

    fixed connect

---
+++ connect.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "HOST: 10.79.4.224     CERT: INT603_Backend_01.ppk"
+CERTS/plink.exe root@10.79.4.224 -i CERTS/INT603_Backend_01.ppk df -h 
+echo "HOST: 10.79.6.95     CERT: INT603_Backend_02.ppk"
+CERTS/plink.exe root@10.79.6.95 -i CERTS/INT603_Backend_02.ppk df -h 
+echo "HOST: 10.79.6.211     CERT: INT603_Backend_03.ppk"
+CERTS/plink.exe root@10.79.6.211 -i CERTS/INT603_Backend_03.ppk df -h 
+echo "HOST: 10.79.5.0     CERT: INT603_Backend_04.ppk"
+CERTS/plink.exe root@10.79.5.0 -i CERTS/INT603_Backend_04.ppk df -h 
+echo "HOST: 10.79.5.76     CERT: INT603_Backend_05.ppk"
+CERTS/plink.exe root@10.79.5.76 -i CERTS/INT603_Backend_05.ppk df -h 
+echo "HOST: 10.79.4.157     CERT: INT603_Backend_06.ppk"
+CERTS/plink.exe root@10.79.4.157 -i CERTS/INT603_Backend_06.ppk df -h 
+echo "HOST: 10.79.7.77     CERT: INT603_Backend_07.ppk"
+CERTS/plink.exe root@10.79.7.77 -i CERTS/INT603_Backend_07.ppk df -h 
+echo "HOST: 10.79.7.14     CERT: INT603_Backend_08.ppk"
+CERTS/plink.exe root@10.79.7.14 -i CERTS/INT603_Backend_08.ppk df -h 
+echo "HOST: 10.79.5.51     CERT: INT603_Backend_09.ppk"
+CERTS/plink.exe root@10.79.5.51 -i CERTS/INT603_Backend_09.ppk df -h 
+echo "HOST: 10.79.4.244     CERT: INT603_Backend_10.ppk"
+CERTS/plink.exe root@10.79.4.244 -i CERTS/INT603_Backend_10.ppk df -h 
+echo "HOST: 10.79.5.37     CERT: INT603_Backend_11.ppk"
+CERTS/plink.exe root@10.79.5.37 -i CERTS/INT603_Backend_11.ppk df -h 
+echo "HOST: 10.79.6.30     CERT: INT603_Backend_12.ppk"
+CERTS/plink.exe root@10.79.6.30 -i CERTS/INT603_Backend_12.ppk df -h 
+echo "HOST: 10.79.5.116     CERT: INT603_Backend_13.ppk"
+CERTS/plink.exe root@10.79.5.116 -i CERTS/INT603_Backend_13.ppk df -h 
+echo "HOST: 10.79.6.221     CERT: INT603_Backend_14.ppk"
+CERTS/plink.exe root@10.79.6.221 -i CERTS/INT603_Backend_14.ppk df -h 
+echo "HOST: 10.79.7.202     CERT: INT603_Backend_15.ppk"
+CERTS/plink.exe root@10.79.7.202 -i CERTS/INT603_Backend_15.ppk df -h 
+echo "HOST: 10.79.4.146     CERT: INT603_Backend_16.ppk"
+CERTS/plink.exe root@10.79.4.146 -i CERTS/INT603_Backend_16.ppk df -h 
+echo "HOST: 10.79.7.151     CERT: INT603_Backend_17.ppk"
+CERTS/plink.exe root@10.79.7.151 -i CERTS/INT603_Backend_17.ppk df -h 
+echo "HOST: 10.79.4.72     CERT: INT603_Backend_18.ppk"
+CERTS/plink.exe root@10.79.4.72 -i CERTS/INT603_Backend_18.ppk df -h 
+echo "HOST: 10.79.5.65     CERT: INT603_Backend_19.ppk"
+CERTS/plink.exe root@10.79.5.65 -i CERTS/INT603_Backend_19.ppk df -h 
+echo "HOST: 10.79.5.13     CERT: INT603_Backend_20.ppk"
+CERTS/plink.exe root@10.79.5.13 -i CERTS/INT603_Backend_20.ppk df -h 
